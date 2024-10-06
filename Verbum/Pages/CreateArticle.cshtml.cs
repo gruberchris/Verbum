a8d76896c6fd66898d91a2434aad307a73be3dc5
Chris Gruber <gruber.christopher@gmail.com>
2024-10-06 15:34:56 -0500

    Replace existing articles.

--- Verbum/Pages/CreateArticle.cshtml.cs
+++ Verbum/Pages/CreateArticle.cshtml.cs
@@ -1,9 +1,11 @@
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Verbum.Pages;
 
-public class CreateArticle : PageModel
+public class CreateArticle(ILogger<SearchResults> logger) : PageModel
 {
     [BindProperty]
     public string ArticleName { get; set; } = string.Empty;
@@ -23,6 +25,16 @@ public class CreateArticle : PageModel
 
         // Ensure the directory exists
         Directory.CreateDirectory(Path.GetDirectoryName(filePath)!);
+        
+        // Check if the file already exists
+        if (System.IO.File.Exists(filePath))
+        {
+            // Generate a unique ID and append it to the filename
+            //var uniqueId = GenerateUniqueId(5);
+            //filePath = Path.Combine("Articles", $"{formattedArticleName}-{uniqueId}.md");
+            logger.LogWarning("Replacing existing article: {ArticleName}", ArticleName);
+            
+        }
 
         // Save the markdown content to a file
         await System.IO.File.WriteAllTextAsync(filePath, MarkdownContent);
@@ -33,5 +45,18 @@ public class CreateArticle : PageModel
     private static string ConvertToSlug(string input)
     {
         return input.Replace(" ", "-").ToLowerInvariant();
+    }
+    
+    private static string GenerateUniqueId(int length)
+    {
+        const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        var data = new byte[length];
+        RandomNumberGenerator.Fill(data);
+        var result = new StringBuilder(length);
+        foreach (var byteValue in data)
+        {
+            result.Append(chars[byteValue % chars.Length]);
+        }
+        return result.ToString();
     }
 }
